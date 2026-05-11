bf8f9362df85f7eae9df38e0bd465f9aa5edb951
Ahmed Marzougui <marzougui.ahmed@esprit.tn>
2026-05-11 07:52:30 +0100

    Delete Modules directory

--- Modules/Signalement/scratch/test_db.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once '../config/config.php';
-$stmt = $pdo->query("SELECT COUNT(*) as total FROM signalement");
-$row = $stmt->fetch();
-echo "Nombre de signalements dans la base : " . $row['total'];
-?>
